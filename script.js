bc6a881fd36d348ba5bbf3449268afe9a9cd2b32
sixunity <68563790+sixunity@users.noreply.github.com>
2023-11-04 17:57:08 +0200

    upgrades ,people, upgrades
    made carousel interactive, also made the guide thingy go away

--- script.js
+++ script.js
@@ -14,36 +14,86 @@ var slideIndex = 0;
 var slides = document.getElementsByClassName("mySlides");
 var container = document.getElementById("imagers");
 var slidesArray = Array.from(slides);
+var carouselInterval;
+var isMouseOver = false;
+
+for (var i = 0; i < slidesArray.length; i++) {
+  slidesArray[i].addEventListener("mouseover", pauseCarousel);
+  slidesArray[i].addEventListener("mouseout", resumeCarousel);
+}
 
 carousel();
 
 function carousel() {
   var i;
   for (i = 0; i < slidesArray.length; i++) {
-    slidesArray[slideIndex].style.opacity = 0.5;
-            slidesArray[slideIndex].style.transform = "scale(1)"; 
-                            slidesArray[slideIndex].style.marginRight = "0px";
-                                slidesArray[slideIndex].style.marginLeft = "0px";
+    slidesArray[i].style.opacity = 0.5;
+    slidesArray[i].style.transform = "scale(1)";
+    slidesArray[i].style.marginRight = "0px";
+    slidesArray[i].style.marginLeft = "0px";
   }
-
 
   slideIndex = (slideIndex + 1) % slidesArray.length;
 
+  slidesArray[slideIndex].style.opacity = 1;
+  slidesArray[slideIndex].style.marginRight = "20px";
+  slidesArray[slideIndex].style.marginLeft = "20px";
+  slidesArray[slideIndex].style.transform = "scale(1.2)";
+  container.classList.remove("changeOrderAnimation");
 
-
-
-    
-    
-console.log(slideIndex);
-        slidesArray[slideIndex].style.opacity = 1;
-                slidesArray[slideIndex].style.marginRight = "20px";
-                
-                                slidesArray[slideIndex].style.marginLeft = "20px";
-        slidesArray[slideIndex].style.transform = "scale(1.2)"; 
-        container.classList.remove("changeOrderAnimation");
-  setTimeout(carousel, 2000);
+  carouselInterval = setTimeout(function () {
+    if (!isMouseOver) {
+      carousel();
+    }
+  }, 2000);
 }
 
+function pauseCarousel(event) {
+  var targetImage = event.target;
+  isMouseOver = true;
+
+  for (var i = 0; i < slidesArray.length; i++) {
+    slidesArray[i].style.opacity = 0.5;
+    slidesArray[i].style.transform = "scale(1)";
+    slidesArray[i].style.marginRight = "0px";
+    slidesArray[i].style.marginLeft = "0px";
+  }
+
+  targetImage.style.opacity = 1;
+  targetImage.style.marginRight = "30px";
+  targetImage.style.marginLeft = "30px";
+  targetImage.style.transform = "scale(1.3)";
+  clearInterval(carouselInterval);
+}
+
+function resumeCarousel(event) {
+  var targetImage = event.target;
+  isMouseOver = false;
+
+  targetImage.style.opacity = 0.5;
+  targetImage.style.marginRight = "0px";
+  targetImage.style.marginLeft = "0px";
+  targetImage.style.transform = "scale(1)";
+
+  carouselInterval = setTimeout(function () {
+    if (!isMouseOver) {
+      carousel();
+    }
+  }, 2000);
+}
+
+
+
+function hidehelp(){
+var hideethis = document.getElementsByClassName("guide");
+const hidethis = document.querySelector(".guide");
+hidethis.ontransitionend = () => {
+hideethis[0].style.opacity=0;
+//hideethis[0].style.visibility="hidden";
+  console.log("Animation ended");
+};
+
+}
 
 
 
